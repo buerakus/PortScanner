0c8427b6a4ef87bcc5267d0e44e2efd670233d3e
buerakus <112691067+buerakus@users.noreply.github.com>
2024-04-25 16:16:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,27 @@ from scapy.layers.inet import TCP, IP
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-if len(sys.argv) != 3:
-    print("The format is: main.py targetIP port_file_path")
+if len(sys.argv) != 5:
+    print("The format is : main.py targetIP startPort endPort bruteforce_file_path")
     sys.exit(0)
 
 target_ip = str(sys.argv[1])
-port_file_path = sys.argv[2]
+startingPort = int(sys.argv[2])
+lastPort = int(sys.argv[3])
+data_path = str(sys.argv[4])
+print("Scanning " + target_ip + " for TCP ports")
 
-print("Scanning {} for TCP ports".format(target_ip))
+if startingPort == lastPort:
+    lastPort += 1
 
-with open(port_file_path, 'r') as port_file:
-    ports = port_file.read().split(', ')
-
-for port in ports:
-    packet = IP(dst=target_ip) / TCP(dport=int(port), flags='S')
-    response = sr1(packet, timeout=2, verbose=0)
-    if response and response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
-        print("Port {} is open!".format(port))
-        if response:
-            sr(IP(dst=target_ip) / TCP(dport=response.sport, flags='R'), timeout=0.5, verbose=0)
+try:
+    for x in range(startingPort,lastPort):
+        packet = IP(dst=target_ip)/TCP(dport=x, flags='S')
+        response = sr1(packet, timeout=0.5, verbose=0)
+        if response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+            print("Port " + str(x) + " is open!")
+        sr(IP(dst=target_ip)/TCP(dport=response.sport,flags='R'), timeout=0.5, verbose=0)
+except AttributeError:
+    print("No more ports available!")
 
 print("Scan is complete!")
